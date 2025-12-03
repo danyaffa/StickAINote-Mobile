36f57bb17b34890af655fb4c3e93a827ec0e863d
Dan Leffler <leffleryd@gmail.com>
2025-12-04 09:01:29 +1100

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,14 @@
+import { CapacitorConfig } from "@capacitor/core";
+
+const config: CapacitorConfig = {
+  appId: "com.stickainote.app",
+  appName: "StickAINote",
+  webDir: "dist",
+  bundledWebRuntime: false,
+  server: {
+    url: "https://stickainote.com",
+    cleartext: false
+  }
+};
+
+export default config;
